a78e33c0baeadf2aeb617e868d32a06beaa0de92
phenriqueribeiroo <160363071+phenriqueribeiroo@users.noreply.github.com>
2024-03-03 15:05:18 -0300

    Update einstein.py

--- CS50P/CS50P/einstein/einstein.py
+++ CS50P/CS50P/einstein/einstein.py
@@ -1,3 +1,15 @@
+# Even if you haven’t studied physics (recently or ever!), you might
+# have heard that E = mc², wherein E represents energy (measured in
+# Joules), m represents mass (measured in kilograms), and c represents
+# the speed of light (measured approximately as 300000000 meters per
+# second), per Albert Einstein et al. Essentially, the formula means
+# that mass and energy are equivalent.
+
+# In a file called einstein.py, implement a program in Python that
+# prompts the user for mass as an integer (in kilograms) and then
+# outputs the equivalent number of Joules as an integer. Assume that the
+# user will input an integer.
+
 m = int(input("m: "))
 c = 300000000
 E = m * (c ** 2)
